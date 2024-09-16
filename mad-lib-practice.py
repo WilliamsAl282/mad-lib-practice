d59c1dd07a03763b1d76c0a0c8555e45561495cf
WilliamsAl282 <WilliamsAl282@northwested.org>
2024-09-16 10:58:59 -0400

    I did some work

---
+++ mad-lib-practice.py
@@ -0,0 +1,21 @@
+noun = input('give noun:\n')
+verb = input('give verb (plural):\n')
+noun2 = input('give a second noun:\n')
+noun2_upper = noun2.upper()
+noun3 = input('give a third noun:\n')
+verb2 = input('give a second verb:\n')
+noun4 = input('give fourth noun:\n')
+verb3 = input('give thrid verb:\n')
+verb3_lower = verb3.lower()
+verb4 = input('give a fourth verb:\n')
+
+
+
+mad_libs = f"Hello, welcome to the city of {noun} in this city it's very unexpected what people do, they could start {verb} or they could be trying to steal {noun2_upper} this wild place could end up with you in the hospital so make sure you bring a {noun3} to defend yourself. This place is kinda like GTA but instead of stealing cars people {verb2} and {verb3_lower}. Finally watch out for wild {noun4} they like to {verb4}"
+print(mad_libs)
+
+
+
+
+
+
